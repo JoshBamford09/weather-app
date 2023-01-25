8e1c00bbbc0a05d663d0253bb2a8cfb0c57b8f51
Josh Bamford <joshbamford09@gmail.com>
2023-01-25 21:23:09 +0000

    Location details: app.js and index updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import '../styles/app.css';
+import React from "react";
+import "../styles/App.css";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
-  return (
-    <div className="prop">
-      <h1>Weather App</h1>
-    </div>
-  );
+function App({ location }) {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
 }
 
 export default App;
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
