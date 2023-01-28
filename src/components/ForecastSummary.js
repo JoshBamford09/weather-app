007a6a75958151576c4d7c3d9973fddd7b1b88c2
Josh Bamford <joshbamford09@gmail.com>
2023-01-28 20:58:30 +0000

    completed test for forcast summary

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -4,15 +4,20 @@ import PropTypes from "prop-types";
 function ForecastSummary(props) {
   const { date, description, icon, temperature } = props;
   return (
-    <div className="forecast-summary">
-      <div className="forecast-summary__date">{date}</div>
-      <div className="forecast-summary__icon">{icon}</div>
-      <div className="forecast-summary__temperature">
-        {temperature.max}
-        &deg;C
+    <>
+      <div className="forecast-summary">
+        <div className="forecast-summary__date">{date}</div>
+        <div className="forecast-summary__icon">{icon}</div>
+        <div className="forecast-summary__temperature">
+          {temperature.max}
+          &deg;C
+        </div>
+        <div className="forecast-summary__description">{description}</div>
       </div>
-      <div className="forecast-summary__description">{description}</div>
-    </div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
+    </>
   );
 }
 
